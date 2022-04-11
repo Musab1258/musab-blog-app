67c926e011449325c749f222859bf337c345a996
Habeeb Musab <habeebmusab@gmail.com>
2022-04-11 12:55:05 +0100

    blog app

---
+++ pages/bio.js
@@ -0,0 +1,10 @@
+const Bio = () => {
+  return (
+    <div className="mt-3">
+      <p className="display-4 text-center">I&#39;m Musab Habeeb</p>
+      <p className="text-center">Musab Habeeb&#39;s bio here</p>
+    </div>
+  )
+}
+
+export default Bio
