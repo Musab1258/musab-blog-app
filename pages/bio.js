ec3c29f44c15320e7c4f6f105c01d9080826463d
Habeeb Musab <habeebmusab@gmail.com>
2022-04-13 23:54:43 +0100

    blog app

--- pages/bio.js
+++ pages/bio.js
@@ -3,6 +3,10 @@ const Bio = () => {
     <div className="mt-3">
       <p className="display-4 text-center">I&#39;m Musab Habeeb</p>
       <p className="text-center">Musab Habeeb&#39;s bio here</p>
+      <p>Hi, my name is Musab Habeeb, I am a frontend web developer.</p>
+      <p>I am currently focused on React.js and it's ecosystem of tools, technologies and frameworks to craft amazing user interfaces.</p>
+      <p>I am enthusiastic about blockchain development and I am looking to transiton into it.</p>
+      <p>I am open to learning new things.</p>
     </div>
   )
 }
